e8f6b87118bc5ca7e2ba57274edfc675af8c202a
emmanuelokoth2002 <emmanuelokoth2002@gmail.com>
2023-05-31 05:40:14 -0700

    Task 1

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -17,10 +17,10 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 	if (new_node == NULL)
 		return (NULL);
 	if (parent->left != NULL)
-		{
-			new_node->left = parent->left;
-			parent->left->parent = new_node;
-			}
+	{
+		new_node->left = parent->left;
+		parent->left->parent = new_node;
+	}
 	parent->left = new_node;
 
 	return (new_node);
